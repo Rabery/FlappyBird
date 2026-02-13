fd14739bf5217742b7a6881a9b090ef9b7f29838
Rabery <raphael.rybowski@gmail.com>
2026-02-13 12:14:12 +0100

    background

--- code/main.py
+++ code/main.py
@@ -1,1 +1,40 @@
-import pygame
+import pygame, time
+from settings import *
+from sprites import BG
+
+class Game:
+	def __init__(self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+		pygame.display.set_caption("Flappy Bird")
+		self.clock = pygame.time.Clock()
+
+		self.allGroup = pygame.sprite.Group()
+		self.collideGroup = pygame.sprite.Group()
+
+		self.background = BG(self.allGroup)
+	
+	def run(self):
+		lastTime = time.time()
+		running = True
+		while running:
+			
+			dt = time.time() - lastTime
+			lastTime = time.time()
+			
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					running = False
+			
+			self.screen.fill("black")
+			self.allGroup.update(dt)
+			self.allGroup.draw(self.screen)
+
+			pygame.display.update()
+			self.clock.tick(FRAMERATE)
+		
+		pygame.quit()
+
+if __name__ == "__main__":
+	game = Game()
+	game.run()
--- code/sprites.py
+++ code/sprites.py
@@ -1,1 +1,18 @@
 import pygame
+from settings import *
+
+class BG(pygame.sprite.Sprite):
+	def __init__(self, groups):
+		super().__init__(groups)
+		bgImage = pygame.image.load("../graphics/environment/background.png").convert()
+		scaleFactor = WINDOW_HEIGHT / bgImage.get_height()
+		self.image = pygame.transform.scale(bgImage, (bgImage.get_width() * scaleFactor, bgImage.get_height() * scaleFactor))
+		self.rect = self.image.get_rect(topleft=(0,0))
+		
+		self.pos = pygame.math.Vector2(self.rect.topleft)
+
+	def update(self, dt):
+		self.pos.x -= 300 * dt
+		if self.rect.right <= 0:
+			self.pos.x = 0
+		self.rect.x = round(self.pos.x)
